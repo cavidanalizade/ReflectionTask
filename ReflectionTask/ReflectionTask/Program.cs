16754afca5d662b2a4352085550d7d42d3fcc2ed
cavidanalizade <145671273+cavidanalizade@users.noreply.github.com>
2023-10-20 17:58:31 +0400

    Add files via upload

---
+++ ReflectionTask/ReflectionTask/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type userType = typeof(User);
+            object userInstance = Activator.CreateInstance(userType);
+            FieldInfo nameProperty = userType.GetField("name", BindingFlags.NonPublic | BindingFlags.Instance );
+            nameProperty.SetValue(userInstance, "Javidan");
+            FieldInfo surnameProperty = userType.GetField("surname", BindingFlags.NonPublic | BindingFlags.Instance);
+            surnameProperty.SetValue(userInstance, "Alizada");
+            FieldInfo ageField = userType.GetField("age", BindingFlags.NonPublic | BindingFlags.Static);
+            ageField.SetValue(userInstance, 19);
+
+            string userName = (string)nameProperty.GetValue(userInstance);
+            string userSurname = (string)surnameProperty.GetValue(userInstance);
+            int userAge = (int)ageField.GetValue(userInstance);
+
+
+             
+            Console.WriteLine("Name: " + userName);
+            Console.WriteLine("Surname: " + userSurname);
+            Console.WriteLine("Age: "+ userAge);
+
+            MethodInfo AgeMethodInfo = userType.GetMethod("ChangeAge", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = new object[] {(byte)35 };
+
+            AgeMethodInfo.Invoke(userInstance, parameters);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ReflectionTask/ReflectionTask/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+
+namespace ReflectionTask
+{
+    class User
+    {
+        private int id;
+        private string name;
+        private string surname;
+        private static int age;
+
+        void GetFullName()
+        {
+            Console.WriteLine(name+" "+surname);
+        }
+        void ChangeAge(int newAge)
+        {
+            age = newAge;
+            Console.WriteLine("New age =" + age);
+        }
+
+    }
+}
